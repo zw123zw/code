258a2102aeb482d75183609266567d08b6289462
zw123zw <zw18674069215@163.com>
2023-03-05 20:50:16 +0800

    提交代码

---
+++ src/utils/index.ts
@@ -0,0 +1,171 @@
+import type { RouteLocationNormalized, RouteRecordNormalized } from 'vue-router';
+import type { App, Plugin } from 'vue';
+
+import { unref } from 'vue';
+import { isObject } from '/@/utils/is';
+
+// update-begin--author:sunjianlei---date:20220408---for: 【VUEN-656】配置外部网址打不开，原因是带了#号，需要替换一下
+export const URL_HASH_TAB = `__AGWE4H__HASH__TAG__PWHRG__`;
+// update-end--author:sunjianlei---date:20220408---for: 【VUEN-656】配置外部网址打不开，原因是带了#号，需要替换一下
+
+export const noop = () => {};
+
+/**
+ * @description:  Set ui mount node
+ */
+export function getPopupContainer(node?: HTMLElement): HTMLElement {
+  return (node?.parentNode as HTMLElement) ?? document.body;
+}
+
+/**
+ * Add the object as a parameter to the URL
+ * @param baseUrl url
+ * @param obj
+ * @returns {string}
+ * eg:
+ *  let obj = {a: '3', b: '4'}
+ *  setObjToUrlParams('www.baidu.com', obj)
+ *  ==>www.baidu.com?a=3&b=4
+ */
+export function setObjToUrlParams(baseUrl: string, obj: any): string {
+  let parameters = '';
+  for (const key in obj) {
+    parameters += key + '=' + encodeURIComponent(obj[key]) + '&';
+  }
+  parameters = parameters.replace(/&$/, '');
+  return /\?$/.test(baseUrl) ? baseUrl + parameters : baseUrl.replace(/\/?$/, '?') + parameters;
+}
+
+export function deepMerge<T = any>(src: any = {}, target: any = {}): T {
+  let key: string;
+  for (key in target) {
+    src[key] = isObject(src[key]) ? deepMerge(src[key], target[key]) : (src[key] = target[key]);
+  }
+  return src;
+}
+
+export function openWindow(url: string, opt?: { target?: TargetContext | string; noopener?: boolean; noreferrer?: boolean }) {
+  const { target = '__blank', noopener = true, noreferrer = true } = opt || {};
+  const feature: string[] = [];
+
+  noopener && feature.push('noopener=yes');
+  noreferrer && feature.push('noreferrer=yes');
+
+  window.open(url, target, feature.join(','));
+}
+
+// dynamic use hook props
+export function getDynamicProps<T, U>(props: T): Partial<U> {
+  const ret: Recordable = {};
+
+  Object.keys(props).map((key) => {
+    ret[key] = unref((props as Recordable)[key]);
+  });
+
+  return ret as Partial<U>;
+}
+
+/**
+ * 获取表单字段值数据类型
+ * @param props
+ * @param field
+ * @updateBy:zyf
+ */
+export function getValueType(props, field) {
+  let formSchema = unref(unref(props)?.schemas);
+  let valueType = 'string';
+  if (formSchema) {
+    let schema = formSchema.filter((item) => item.field === field)[0];
+    valueType = schema.componentProps && schema.componentProps.valueType ? schema.componentProps.valueType : valueType;
+  }
+  return valueType;
+}
+export function getRawRoute(route: RouteLocationNormalized): RouteLocationNormalized {
+  if (!route) return route;
+  const { matched, ...opt } = route;
+  return {
+    ...opt,
+    matched: (matched
+      ? matched.map((item) => ({
+          meta: item.meta,
+          name: item.name,
+          path: item.path,
+        }))
+      : undefined) as RouteRecordNormalized[],
+  };
+}
+/**
+ * 深度克隆对象、数组
+ * @param obj 被克隆的对象
+ * @return 克隆后的对象
+ */
+export function cloneObject(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+export const withInstall = <T>(component: T, alias?: string) => {
+  const comp = component as any;
+  comp.install = (app: App) => {
+    app.component(comp.name || comp.displayName, component);
+    if (alias) {
+      app.config.globalProperties[alias] = component;
+    }
+  };
+  return component as T & Plugin;
+};
+
+/**
+ * 获取url地址参数
+ * @param paraName
+ */
+export function getUrlParam(paraName) {
+  let url = document.location.toString();
+  let arrObj = url.split('?');
+
+  if (arrObj.length > 1) {
+    let arrPara = arrObj[1].split('&');
+    let arr;
+
+    for (let i = 0; i < arrPara.length; i++) {
+      arr = arrPara[i].split('=');
+
+      if (arr != null && arr[0] == paraName) {
+        return arr[1];
+      }
+    }
+    return '';
+  } else {
+    return '';
+  }
+}
+
+/**
+ * 休眠（setTimeout的promise版）
+ * @param ms 要休眠的时间，单位：毫秒
+ * @param fn callback，可空
+ * @return Promise
+ */
+export function sleep(ms: number, fn?: Fn) {
+  return new Promise<void>((resolve) =>
+    setTimeout(() => {
+      fn && fn();
+      resolve();
+    }, ms)
+  );
+}
+
+/**
+ * 不用正则的方式替换所有值
+ * @param text 被替换的字符串
+ * @param checker  替换前的内容
+ * @param replacer 替换后的内容
+ * @returns {String} 替换后的字符串
+ */
+export function replaceAll(text, checker, replacer) {
+  let lastText = text;
+  text = text.replace(checker, replacer);
+  if (lastText !== text) {
+    return replaceAll(text, checker, replacer);
+  }
+  return text;
+}
